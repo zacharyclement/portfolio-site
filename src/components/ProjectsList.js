e43f533214c5e3ea65df2ac3e686ea9b0ab2d438
zachclem42@gmail.com <ZCC@ZachsMachine.fios-router.home>
2018-02-12 09:42:34 -0500

    finish v1 of zachclement.com

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -6,9 +6,9 @@ import '../App.css';
 import projects from '../data';
 import scrollButton from '../images/downArrowBlack.svg';
 
-const BackgroundImage = 'https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg';
+const BackgroundImage = 'https://ak9.picdn.net/shutterstock/videos/6968029/thumb/1.jpg?i10c=img.resize(height:160)';
 const buttonStyle = {
-  background: 'none', padding: 20, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', width: '40px',
+  background: 'none', padding: 20, position: 'absolute', top: '80%', left: '50%', transform: 'translate(-50%,-50%)', width: '40px',
 };
 const backgroundImageStyle = {
   display: 'grid', objectFit: 'cover', gridArea: '1/1/-1/-1', height: '100vh', objectPosition: '', width: '100%',
@@ -20,13 +20,12 @@ const ProjectsList = () => (
 
 
     <Parallax
-      strength={100}
+      strength={300}
       bgImage={BackgroundImage}
       blur={-5}
     >
       <div style={backgroundImageStyle}>
         <input type="image" src={scrollButton} style={buttonStyle} onClick={() => scrollToComponent(this.scrollArea, { offset: 1 })} />
-
       </div>
 
     </Parallax>
